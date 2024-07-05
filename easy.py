a4bc64a901b3befd1693aaa8ce9310ac940c7ae8
jacpa2011 <156595952+jacpa2011@users.noreply.github.com>
2024-07-05 18:23:57 +0200

    2nd twice effecient and 156

--- easy.py
+++ easy.py
@@ -1,5 +1,7 @@
 while True:
+    y = input("input number:  ")
+    y = y.lower().replace("illion", "o")
     x = input("input number:  ")
     x = x.lower().replace("illion", "o")
-
-    print(f"centinoni{x}-decinoni{x}-noni{x}-")
+    
+    print(f"centinoni{x}-decinoni{x}-noni{x}-centinoni{y}-decinoni{y}-noni{y}-")
